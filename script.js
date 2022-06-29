327beb3bfb4ef920266a7446ef7fd54b2b28cd1d
Lulu <lulukapita@gmail.com>
2022-06-28 20:18:32 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let questions = [
+    {
+        number 1: "What does HTML stand for?",
+        choices: 
+        ["Hyper Text Markup Language", 
+        "Hyper Teen Madeup  Language", 
+        "Hard Trained Machine Language", 
+        "Hybrid Text Media Language"],
+        answer: "Hyper Text Markup Language",
+        },
+    {  
+        number 2: "What does CSS stand for?",
+        choices: 
+        ["Cascading Style Sheet", 
+        "Creating Style Sheet", 
+        "Creative Styles Sheet", 
+        "Crazy Super Styles"],
+        answer: "Cascading Style Sheet",
+        },  
+    {
+        number 3: "What does JS stand for?",
+        choices:
+        ["Jar Script",
+        "Jello Store",
+        "Javascript",
+        "JustSimple"],
+        }   
